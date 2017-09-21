b6101a2bcc50ab9416d919bdc7dd57fd4a418c21
Unix&Me <unixnme@gmail.com>
2017-09-22 01:14:21 +0900

    densenet

--- params.py
+++ params.py
@@ -1,14 +1,15 @@
 from model.u_net import get_unet_128, get_unet_256, get_unet_512, get_unet_1024
+from model.densenet import dense_net_128
 
-rows = 1024
-cols = 1024 / 2 * 3
+rows = 128
+cols = 128
 
 max_epochs = 100
-batch_size = 2
+batch_size = 8
 
 orig_width = 1918
 orig_height = 1280
 
 threshold = 0.5
 
-model_factory = get_unet_1024
+model_factory = dense_net_128
