ce4ad0f1e3d106708a616a6f1d4f35bfde8f28a0
Unix&Me <unixnme@gmail.com>
2017-09-23 00:07:40 +0900

    update minor things

--- params.py
+++ params.py
@@ -1,11 +1,11 @@
 from model.u_net import get_unet_128, get_unet_256, get_unet_512, get_unet_1024
 from model.densenet import dense_net_128
 
-rows = 960
-cols = 640
+cols = 960
+rows = 640
 
 max_epochs = 100
-batch_size = 8
+batch_size = 15
 
 orig_width = 1918
 orig_height = 1280
