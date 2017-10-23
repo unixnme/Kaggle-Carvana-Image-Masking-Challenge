6edc746cf4498f06cfeb98e62203503af7a61d2c
Unix&Me <unixnme@gmail.com>
2017-10-23 22:29:19 +0900

    increase block number

--- train.py
+++ train.py
@@ -182,7 +182,7 @@ if __name__ == '__main__':
     learning_rate = 2e-3
     input_mean = 0.
     decay = 0.5
-    offset = 621
+    offset = 631
 
     df_train = pd.read_csv('input/train_masks.csv')
     ids_train = df_train['img'].map(lambda s: s.split('.')[0])
@@ -206,9 +206,9 @@ if __name__ == '__main__':
             filepath = 'weights/' + name + '_model.h5'
 
             model = create_model(shape=(None, None, 3),
-                                 num_blocks=3,
+                                 num_blocks=4,
                                  kernel=3,
-                                 filter=8,
+                                 filter=4,
                                  encoding_dilation=1,
                                  decoding_dilation=1,
                                  regularizer=l2(weight_decay[idx]),
