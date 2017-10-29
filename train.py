1e30d6b52143856ecf85d60e498d7ea2643b70fe
Unix&Me <unixnme@gmail.com>
2017-10-29 21:05:58 +0900

    filter in the reverse direction

--- train.py
+++ train.py
@@ -182,7 +182,7 @@ if __name__ == '__main__':
     learning_rate = 2e-3
     input_mean = 0.
     decay = 0.5
-    offset = 711
+    offset = 741
 
     df_train = pd.read_csv('input/train_masks.csv')
     ids_train = df_train['img'].map(lambda s: s.split('.')[0])
@@ -208,7 +208,7 @@ if __name__ == '__main__':
             model = create_model(shape=(None, None, 3),
                                  num_blocks=3,
                                  kernel=3,
-                                 filter=[16,16,16,16,16,16,16],
+                                 filter=[32,16,8,4,8,16,32],
                                  encoding_dilation=1,
                                  decoding_dilation=1,
                                  regularizer=l2(weight_decay[idx]),
